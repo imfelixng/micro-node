c50b8d110baf116b772f22cc26f64470889844ba
An Ng. Quang <ngquangan@gmail.com>
2020-05-03 11:18:34 +0700

    Config ingress nginx

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.get("/api/users/currentuser", (req: Request, res: Response) => {
+  res.send("Hi there!");
+});
 
 app.listen(3000, () => {
     console.log('Auth service starting at port 3000!!!!');
