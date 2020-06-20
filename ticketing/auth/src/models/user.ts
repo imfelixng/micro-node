641069a088cc4b63883e93ced31dade9fb2a67d2
An Ng. Quang <ngquangan@gmail.com>
2020-06-20 15:25:28 +0700

    Format response data for sign up

--- ticketing/auth/src/models/user.ts
+++ ticketing/auth/src/models/user.ts
@@ -20,16 +20,29 @@ interface UserDoc extends mongoose.Document {
     // updatedAt: string;
 }
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
-    }
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;        
+        return ret;
+      },
+    },
+  }
+);
 
 const buildUser = (attrs: UserAttrs) => {
     return new User(attrs);
