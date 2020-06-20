8f8ae245487413e2de235d6490bd80a99a8b575e
An Ng. Quang <ngquangan@gmail.com>
2020-06-20 17:17:06 +0700

    Create require auth middleware

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
 import { currentUser } from "../middlewares/current-user";
+import { requireAuth } from '../middlewares/require-auth';
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser,(req: Request, res: Response) => {
+router.get("/api/users/currentuser", currentUser, requireAuth, (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null })
 });
 
