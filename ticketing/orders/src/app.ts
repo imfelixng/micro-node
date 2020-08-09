aa677a044aaeaeb0b19b116de483dfc7f768f4d4
An Ng. Quang <an.nguyen@fiisoft.com>
2020-08-09 08:29:52 +0700

    Create orders module

---
+++ ticketing/orders/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@anqtickets/common';
+
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+}));
+
+app.use(currentUser);
+
+
+
+app.all('*', async () => {
+    // Must be handle case async function. Because don't return any promise.
+    // We can use next middleware or express-async-errors package to handle this case.
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export {
+    app
+};
