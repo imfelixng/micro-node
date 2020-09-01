c22d9046f1b2364723e484cfdbdd5088ccb893e0
An Ng. Quang <an.nguyen@fiisoft.com>
2020-09-02 00:42:00 +0700

    Create order cancelled event test

--- ticketing/payments/src/events/listeners/order-cancelled-listener.ts
+++ ticketing/payments/src/events/listeners/order-cancelled-listener.ts
@@ -1,13 +1,26 @@
-import { Listener, OrderCancelledEvent, Subjects } from "@anqtickets/common";
+import { Listener, OrderCancelledEvent, Subjects, OrderStatus } from "@anqtickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from './queue-group-name';
+import { Order } from "../../models/order";
 
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
     subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
     queueGroupName = queueGroupName;
     async onMessage(data: OrderCancelledEvent['data'], msg: Message): Promise<void> {
-        const { ticket: { id: ticketId }} = data;
+        const order = await Order.findOne({
+            _id: data.id,
+            version: data.version - 1
+        });
+
+        if (!order) {
+            throw new Error('Order not found');
+        }
+
+        order.set({
+            status: OrderStatus.CANCELLED
+        });
+        await order.save();
 
         msg.ack();
     }
