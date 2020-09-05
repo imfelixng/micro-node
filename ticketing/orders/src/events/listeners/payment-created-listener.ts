ebb3c236f7eacf00fcd6fe1fdf5d45216e7bec0b
Nguyễn Quang An <ngquangan@gmail.com>
2020-09-05 17:40:54 +0700

    Created payment created listener

---
+++ ticketing/orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,21 @@
+import { Listener, PaymentCreatedEvent, Subjects, OrderStatus } from "@anqtickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from './queue-group-name';
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+    subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+    queueGroupName = queueGroupName;
+    async onMessage(data: PaymentCreatedEvent['data'], msg: Message): Promise<void> {
+        const { orderId } = data;
+        const order = await Order.findById(orderId);
+
+        order.set({
+            status: OrderStatus.COMPLETED
+        });
+        await order.save();
+
+        msg.ack();
+    }
+    
+}
